da355abf3cca64a94148505c2ea2fa69cceffbfa
Aliane Sebastien <sebastien.aliane@student-cs.fr>
2025-06-05 12:55:37 +0200

    test commit

--- centrale-ei-web/backend/datasource.js
+++ centrale-ei-web/backend/datasource.js
@@ -1,6 +1,6 @@
 import { DataSource } from 'typeorm';
 import User from './entities/user.js';
-import Movie from './entities/movie.js'; 
+import Movie from './entities/movie.js';
 import Review from './entities/review.js';
 import 'reflect-metadata';
 
@@ -8,7 +8,8 @@ export const appDataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   synchronize: true,
-  entities: [User, Movie, Review], 
+  entities: [User, Movie, Review],
   logging: true,
   migrations: ['./src/migrations/*.js'],
 });
+
